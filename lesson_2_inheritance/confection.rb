e56fd19414ce2372aeae3faee550ba0bf7dc0b1e
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-08-05 20:19:45 +0800

    refactored

--- lesson_2_inheritance/confection.rb
+++ lesson_2_inheritance/confection.rb
@@ -1,17 +1,13 @@
 # We have a Confection class representing baked desserts. Your job is to create a new subclass of Confection, named Cupcake and Banana Cake. Cupcakes and Banana Cakes need to be baked just like other confections, but cupcakes also need to be frosted afterward. Write aa prepare method for your Cupcke and Banana Cake class that prints "Baking at 350 degrees for 25 minutes.", and then prints "Applying frosting" for Cupcake only.
 
-module Bake
+
+class Confection
   def bake
     "Baking at 350 degrees for 25 minutes."
   end
 end
 
-class Confection
-
-end
-
 class Cupcake < Confection
-  include Bake
   def frost
     "Applying frosting"
   end
@@ -23,7 +19,6 @@ class Cupcake < Confection
 end
 
 class BananaCake < Confection
-  include Bake
   def prepare
     puts bake
   end
